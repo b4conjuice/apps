de0a36850498ff9f821047f3f42f74929f7b31dc
Daniel Lopez <d@niel.to>
2025-06-16 21:25:21 -0700

    style: adjust background color of app and dock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { apps, dockApps } from '@/lib/config'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center bg-black p-8'>
+    <main className='bg-cobalt flex min-h-screen flex-col items-center p-8'>
       <div className='relative flex h-full w-full max-w-[390px] flex-col gap-8'>
         {/* App Grid */}
         <div className='mx-0 grid flex-1 grid-cols-4 gap-x-4 gap-y-8'>
@@ -19,7 +19,7 @@ export default function Home() {
         </div>
 
         {/* Dock */}
-        <div className='mx-0 mb-0 flex h-[85px] items-center rounded-3xl bg-white/20 backdrop-blur-xl'>
+        <div className='bg-cb-dusty-blue mx-0 mb-0 flex h-[85px] items-center rounded-3xl backdrop-blur-xl'>
           <div className='grid w-full grid-cols-4 gap-x-4 px-1'>
             {dockApps.map((app, index) => (
               <div key={index} className='flex justify-center'>
